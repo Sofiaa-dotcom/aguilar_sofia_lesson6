74189c20886e458c67dbd32dfaf101798b3b8474
Sofiaa-dotcom <72768448+Sofiaa-dotcom@users.noreply.github.com>
2021-01-12 11:24:02 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-
+while True:
+    if input.sound_level() > 2:
+         light.set_all(color.rgb(0, 180, 255))
+    else: 
+        light.clear()
--- main.ts
+++ main.ts
@@ -1,1 +1,8 @@
-
+while (true) {
+    if (input.soundLevel() > 2) {
+        light.setAll(color.rgb(0, 180, 255))
+    } else {
+        light.clear()
+    }
+    
+}
